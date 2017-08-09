32f4616cfef1658386d085c0963bbdd9855d1a24
yury1202 <minluying@gmail.com>
2017-08-08 17:41:26 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//using express with node js
+var express = require('express');
+
+//initialize app as an express application
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname+'/public'));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
