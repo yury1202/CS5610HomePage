cfedf3b3d4daba6984b11481fce36e3b66a576bb
yury1202 <minluying@gmail.com>
2017-08-08 17:56:58 -0700

    homepage

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 //initialize app as an express application
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8888));
 app.use(express.static(__dirname+'/public'));
 
 app.listen(app.get('port'), function() {
